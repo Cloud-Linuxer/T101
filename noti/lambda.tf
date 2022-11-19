0f47b21cb7d3ef704eab74a385a3738eeac560bb
Linuxer <linuxer.name@gmail.com>
2022-11-19 20:17:03 +0900

    lamdba permission add

--- noti/lambda.tf
+++ noti/lambda.tf
@@ -1,15 +1,15 @@
 module "lambda_function" {
   source = "terraform-aws-modules/lambda/aws"
 
-  function_name = "AWS-TO-CLACK"
+  function_name = "AWS-TO-SLACK"
   description   = "AWS to slack Noti"
   handler       = "lambda_function.lambda_handler"
   runtime       = "python3.8"
-
-  source_path = "./src/lambda-function"
+  
+source_path = "./src/lambda-function"
   environment_variables = {
-    HOOK_URL = ""
-    SLACK_CHANNEL = ""
+    HOOK_URL = "https://hooks.slack.com/services/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"
+    SLACK_CHANNEL = "test"
   }
 
   tags = {
@@ -18,85 +18,3 @@ module "lambda_function" {
 }
 
 
-resource "aws_sns_topic" "ec2_updates" {
-  name = "user-updates-topic"
-}
-resource "aws_sns_topic_subscription" "ec2_updates_target" {
-  topic_arn = aws_sns_topic.ec2_updates.arn
-  protocol  = "lambda"
-  endpoint  = module.lambda_function.lambda_function_arn
-}
-
-resource "aws_sns_topic_policy" "default" {
-  arn = aws_sns_topic.ec2_updates.arn
-  policy = data.aws_iam_policy_document.sns_topic_policy.json
-}
-
-data "aws_iam_policy_document" "sns_topic_policy" {
-  policy_id = "__default_policy_ID"
-
-  statement {
-    actions = [
-        "SNS:GetTopicAttributes",
-        "SNS:SetTopicAttributes",
-        "SNS:AddPermission",
-        "SNS:RemovePermission",
-        "SNS:DeleteTopic",
-        "SNS:Subscribe",
-        "SNS:ListSubscriptionsByTopic",
-        "SNS:Publish"
-    ]
-    effect = "Allow"
-
-    principals {
-      type        = "Service"
-      identifiers = ["events.amazonaws.com"]
-    }
-
-    resources = [
-      aws_sns_topic.ec2_updates.arn,
-    ]
-
-    sid = "__default_statement_ID"
-  }
-}
-
-
-module "eventbridge" {
-  source = "terraform-aws-modules/eventbridge/aws"
-  create_bus = false
-  rules = {
-    orders = {
-      description   = "Capture all order data"
-      event_pattern = jsonencode({
-  "source": [
-    "aws.ec2"
-  ],
-  "detail-type": [
-    "AWS API Call via CloudTrail"
-  ],
-  "detail": {
-    "eventSource": [
-      "ec2.amazonaws.com"
-    ],
-    "eventName": [
-      "AuthorizeSecurityGroupIngress",
-      "AuthorizeSecurityGroupEgress",
-      "RevokeSecurityGroupIngress",
-      "RevokeSecurityGroupEgress"
-    ]
-  }
-})
-      enabled       = true
-    }
-  }
-
-  targets = {
-    orders = [
-      {
-        name = "log-orders-to-cloudwatch"
-        arn  = aws_sns_topic.ec2_updates.arn
-      }
-    ]
-  }
-}
--- noti/trail.tf
+++ noti/trail.tf
@@ -45,4 +45,5 @@ resource "aws_cloudtrail" "linuxer-cloudtrail" {
   s3_bucket_name                = aws_s3_bucket.linuxer-bucket.id
   s3_key_prefix                 = "prefix"
   include_global_service_events = false
+  depends_on = [aws_s3_bucket_policy.linuxer-bucket]
 }
